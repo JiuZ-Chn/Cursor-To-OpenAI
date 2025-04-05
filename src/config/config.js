031ea505d74e59b3e784626ee911cbd15ddb2a0e
JiuZ <jiuz.chn@outlook.com>
2025-04-05 18:13:37 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
         enabled: false,
         url: 'http://127.0.0.1:7890',
     },
-    chatMode: 1 // 1 for ask, 2 for agent, 3 for edit
+    //chatMode: 1 // 1 for ask, 2 for agent, 3 for edit
 };
