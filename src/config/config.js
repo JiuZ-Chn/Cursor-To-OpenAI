60ca7241d2395a7b19c3c0251ac4d883bdb5569a
qinke <qing4839@gmail.com>
2025-12-30 13:26:19 +0800

    Update Cursor client version to 3.0.0 and enable auto-build Docker image

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    port: process.env.PORT || 3010,
+    proxy:{
+        enabled: false,
+        url: 'http://127.0.0.1:7890',
+    },
+    cursorClientVersion: process.env.CURSOR_CLIENT_VERSION || "3.0.0",
+    //chatMode: 1 // 1 for ask, 2 for agent, 3 for edit
+};
